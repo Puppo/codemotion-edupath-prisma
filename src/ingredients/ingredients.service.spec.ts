14d63c35f7a1d911b3d7e85e9c9a129665cdf69c
Luca Del Puppo <luca@delpuppo.net>
2022-05-06 18:32:24 +0200

    08 Unit Test

--- src/ingredients/ingredients.service.spec.ts
+++ src/ingredients/ingredients.service.spec.ts
@@ -1,18 +1,40 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Ingredient } from '@prisma/client';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { IngredientsService } from './ingredients.service';
 
 describe('IngredientsService', () => {
+  let prismaService: DeepMockProxy<PrismaService>;
   let service: IngredientsService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [IngredientsService],
-    }).compile();
-
-    service = module.get<IngredientsService>(IngredientsService);
+    prismaService = mockDeep<PrismaService>();
+    service = new IngredientsService(prismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should run create ingredient', () => {
+    prismaService.ingredient.create.mockResolvedValue({
+      id: 1,
+      name: 'Ingredient',
+    } as Ingredient);
+
+    expect(service.insert('Ingredient Input')).resolves.toEqual({
+      id: 1,
+      name: 'Ingredient',
+    });
+    expect(prismaService.ingredient.create.mock.calls.length).toBe(1);
+    expect(prismaService.ingredient.create.mock.calls[0][0]).toEqual({
+      data: {
+        name: 'Ingredient Input',
+      },
+      select: {
+        id: true,
+        name: true,
+      },
+    });
   });
 });
